7936d5fa5c020390cd2d1b418b8da5e7ede270e7
EchoFan <Echo.fan@grapecity.com>
2023-06-04 13:10:56 +0900

    add more image variants

--- test-import/index.js
+++ test-import/index.js
@@ -3,4 +3,5 @@ const { IndexFlatL2 } = require('faiss-node');
 
 const dimension = 4096;
 const index = new IndexFlatL2(dimension);
-assert.equal(index.getDimension(), dimension)
+assert.equal(index.getDimension(), dimension);
+console.log(index.getDimension());
