1e9be6747b7962ac3de687cc57a48909568a6d44
Fan <ewfian@users.noreply.github.com>
2023-06-15 21:55:52 +0900

    CI: add buildx for check import on arm64 (#18)
    * test buildx
    
    * test buildx build
    
    * fix test failed
    
    * fix inspect
    
    * test
    
    * fix
    
    * test
    
    * test
    
    * test
    
    * fix
    
    * add logs
    
    * test install
    
    * fix install
    
    * test
    
    * fix
    
    * diable armv7 test

--- test-import/index.js
+++ test-import/index.js
@@ -4,4 +4,8 @@ const { IndexFlatL2 } = require('faiss-node');
 const dimension = 4096;
 const index = new IndexFlatL2(dimension);
 assert.equal(index.getDimension(), dimension);
+
+console.log(process.version);
+console.log(process.platform);
+console.log(process.arch);
 console.log(index.getDimension());
